7157b200413017f8a7ff20ae8b23817333616fe0
Pasan Kalhara <kalharap84@gmail.com>
2026-07-05 11:28:40 +0530

    Complete UserRepo

--- backend/src/main/java/com/example/backend/repository/UserRepo.java
+++ backend/src/main/java/com/example/backend/repository/UserRepo.java
@@ -1,4 +1,12 @@
 package com.example.backend.repository;
 
-public interface UserRepo {
+import com.example.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+    Optional<User> findUserByUsername(String username);
 }
