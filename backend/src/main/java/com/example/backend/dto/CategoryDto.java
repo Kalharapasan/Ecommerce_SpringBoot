d1b15d8d482ba328cc6d16273ce7a337fdc4ffcf
Pasan Kalhara <kalharap84@gmail.com>
2026-07-06 23:19:03 +0530

    Complete CategoryDto

---
+++ backend/src/main/java/com/example/backend/dto/CategoryDto.java
@@ -0,0 +1,17 @@
+package com.example.backend.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CategoryDto {
+    private Integer categoryId;
+    private @NotBlank String categoryName;
+    private @NotNull String description;
+    private @NotNull String imageUrl;
+}
