4468cbd2613bd0ab6fd8d4afca43dd1efc31a0fc
Pasan Kalhara <kalharap84@gmail.com>
2026-07-06 23:24:44 +0530

    Complete ResponseDto

---
+++ backend/src/main/java/com/example/backend/dto/custom/ResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.backend.dto.custom;
+public class ResponseDto {
+    private String message;
+    private Object data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
