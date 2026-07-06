7c953f2a3110151b1f4374857244e0c9f2a7d22d
Pasan Kalhara <kalharap84@gmail.com>
2026-07-06 23:20:36 +0530

    Complete UserDto

---
+++ backend/src/main/java/com/example/backend/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.example.backend.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDto {
+
+    private Integer userId;
+    private String fullName;
+    private String email;
+    private String username;
+    private String password;
+    private String role;
+
+}
