c4ffc0ea6878fbd2aaa97c0ba541bba02be6aef0
Pasan Kalhara <kalharap84@gmail.com>
2026-07-06 23:27:07 +0530

    Complete ProductController

---
+++ backend/src/main/java/com/example/backend/controller/ProductController.java
@@ -0,0 +1,49 @@
+package com.example.backend.controller;
+import com.example.backend.dto.ProductDto;
+import com.example.backend.dto.custom.ResponseDto;
+import com.example.backend.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> createProduct(@RequestBody ProductDto product) {
+        ResponseEntity response = productService.createProduct(product);
+        return response;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseDto> getProductList() {
+        ResponseEntity response = productService.getProductList();
+        return response;
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ResponseDto> getProductById(@PathVariable Integer productId) {
+        ResponseEntity response = productService.getProductById(productId);
+        return response;
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseDto> updateProduct(@RequestBody ProductDto product) {
+        ResponseEntity response = productService.updateProduct(product);
+        return response;
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Integer productId) {
+        ResponseEntity response = productService.deleteProduct(productId);
+        return response;
+    }
+
+}
